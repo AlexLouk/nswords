42835c6aa4a26751844ee2ffbe9a9a1db0c7f976
Alexandros Loukaridis <AlexLouk@icloud.com>
2025-07-09 00:01:06 +0200

    Converted json file

--- tools/json-converter.js
+++ tools/json-converter.js
@@ -1,1 +1,17 @@
-const json = { key1: 'value1', key2: 'value2', key3: 'value3' }
+//Run this Script from the folder that u want the words_en.json to be written to.
+console.time('Transform')
+const fs = require('fs')
+
+const inputPath = process.argv[2]
+if (!inputPath) return console.error('Usage: node file.js <input.json>')
+
+const json = JSON.parse(fs.readFileSync(inputPath, 'utf-8'))
+console.log(json)
+const transformed = Object.entries(json).map(([key, definition]) => ({
+    key,
+    definition,
+}))
+
+fs.writeFileSync('words_en.json', JSON.stringify(transformed, null, 2))
+
+console.timeEnd('Transform')
